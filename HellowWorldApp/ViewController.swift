3b4a445002fa2d667c61d2fad20324ac4ce1cd6b
Nikolay <halemhood@icloud.com>
2022-02-11 21:24:41 +0300

    add button logic

--- HellowWorldApp/ViewController.swift
+++ HellowWorldApp/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var hellowLable: UILabel!
+    @IBOutlet var startButton: UIButton!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        hellowLable.isHidden = true
+        startButton.layer.cornerRadius = 15
     }
 
-
+    @IBAction func startButtonPressed() {
+        if hellowLable.isHidden {
+            hellowLable.isHidden = false
+            startButton.setTitle("Cler Text", for: .normal)
+        } else {
+            hellowLable.isHidden = true
+            startButton.setTitle("Show Text", for: .normal)
+        }
+    }
+    
 }
 
